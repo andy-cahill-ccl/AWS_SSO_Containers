8df16a7e8931db7115e6db8117daf5552cba66e7
andy-cahill-ccl <120691192+andy-cahill-ccl@users.noreply.github.com>
2024-01-11 16:24:51 +1300

    Update options.js

--- options.js
+++ options.js
@@ -8,13 +8,17 @@ let examples = {
 function saveOptions(e) {
   e.preventDefault();
   browser.storage.sync.set({
-    template: document.querySelector("#template").value
+    template: document.querySelector("#template").value,
+    truncate: document.querySelector("#truncate").checked
   });
 }
 
-function populatePreview(text) {
+function populatePreview(text, truncate) {
   for (const [key, value] of Object.entries(examples)) {
     text = text.replace(key, value);
+  }
+  if (text.length > 32 && truncate) {
+    text = text.substring(0, 32) + "&hellip;";
   }
   document.querySelector("#preview").value = text;
 }
@@ -22,20 +26,25 @@ function populatePreview(text) {
 function restoreOptions() {
   function setCurrentChoice(result) {
     let text = result.template || "name role";
+    let truncate = result.truncate;
     document.querySelector("#template").value = text;
-    populatePreview(text);
+    document.querySelector("#truncate").checked = truncate;
+    populatePreview(text, truncate);
   }
 
   function onError(error) {
     console.log(`Error: ${error}`);
   }
 
-  let getting = browser.storage.sync.get("template");
+  let getting = browser.storage.sync.get("template", "truncate");
   getting.then(setCurrentChoice, onError);
 }
 
 document.addEventListener("DOMContentLoaded", restoreOptions);
 document.querySelector("form").addEventListener("submit", saveOptions);
 document.querySelector("#template").addEventListener("input", function(evt) {
-  populatePreview(this.value);
+  populatePreview(this.value, document.querySelector("#truncate").checked);
+});
+document.querySelector("#truncate").addEventListener("input", function(evt) {
+  populatePreview(document.querySelector("#template").value, this.checked);
 });
